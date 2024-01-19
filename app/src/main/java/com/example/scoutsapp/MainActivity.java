b5b576a8a27f62eeb3b7181f1276a55303e6f123
radin <s16131.nhs.co.il>
2024-01-19 12:14:31 +0200

    added variables for the login

--- app/src/main/java/com/example/scoutsapp/MainActivity.java
+++ app/src/main/java/com/example/scoutsapp/MainActivity.java
@@ -3,13 +3,46 @@ package com.example.scoutsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText etEmail, etPassword;
+    private Button btnLogin;
+    private TextView tvSignUp, tvForgot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvSignUp = findViewById(R.id.tvSignUp);
+        tvForgot = findViewById(R.id.tvForgot);
+
+        btnLogin.setOnClickListener(this);
+        tvSignUp.setOnClickListener(this);
+        tvForgot.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == btnLogin)
+        {
+
+        }
+        else if(view == tvSignUp)
+        {
+
+        }
+        else if(view == tvForgot)
+        {
+
+        }
     }
 }
